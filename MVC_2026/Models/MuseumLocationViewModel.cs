b93d91a56bec6d6401621e92358da2a26efe24e1
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-07 17:30:09 +0530

    Add MuseumLocationViewModel class
    Introduces MuseumLocationViewModel with properties for museum ID, name, latitude, and longitude to represent museum location data.

---
+++ MVC_2026/Models/MuseumLocationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MapLocation.Models
+{
+    public class MuseumLocationViewModel
+    {
+        public int MuseumId { get; set; }
+        public string MuseumName { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
