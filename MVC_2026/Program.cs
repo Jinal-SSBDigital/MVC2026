429280b3b3816db1f9dcf6ec7df49020c6f7a20a
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-07 17:31:00 +0530

    Add initial ASP.NET Core app setup with Google auth
    Created Program.cs to configure services, including SQL Server DbContext, Google authentication, and MVC routing. Sets up default route to Account/Login and enables HTTPS, static files, and authorization middleware.

---
+++ MVC_2026/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MVC2026.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbCon")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = "547069491176-rs7qkshfeoubgsm5ibj670l22jal83dq.apps.googleusercontent.com";
+    options.ClientSecret = "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";
+    options.CallbackPath = "/signin-google"; // default
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+app.Run();
