54e1a39739fc1e982386e00d7e50a488993739d1
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-07 17:29:43 +0530

    Add Museum controller with Location action
    Introduces a new Museum controller that fetches museum location data from an external API and returns it to the view. Utilizes IHttpClientFactory for HTTP requests and deserializes the response into a MuseumLocationViewModel.

---
+++ MVC_2026/Controllers/Museum.cs
@@ -0,0 +1,27 @@
+﻿using MapLocation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MapLocation.Controllers
+{
+    public class Museum : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public Museum(IHttpClientFactory httpfact)
+        {
+            _httpClient = httpfact.CreateClient();
+        }
+        public async Task<IActionResult> Location(int id)
+        {
+            var response = await _httpClient.GetAsync($"https://localhost:7129/api/MuseumLocation?MId=39");
+            //var response = await _httpClient.GetAsync($"https://localhost:5001/api/museum/location/{id}");
+            if(!response.IsSuccessStatusCode)
+                return View("Error");
+
+            var json=await response.Content.ReadAsStringAsync();
+            var location= JsonConvert.DeserializeObject<MuseumLocationViewModel>(json);
+            return View(location);
+        }
+    }
+}
