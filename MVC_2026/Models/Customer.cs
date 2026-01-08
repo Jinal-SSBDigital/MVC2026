5f116f2113027bc8a2ec8b130dfdde643baf65f9
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-08 17:52:16 +0530

    Add Customer model class
    Introduces the Customer model with properties for ID, name, email, mobile, password fields, address, and creation date. This model will be used to represent customer data in the application.

---
+++ MVC_2026/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC2026.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustmId { get; set; }
+
+        public string Custm_name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string PlainTextPassword { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Address { get; set; }
+
+        public DateTime Createddate { get; set; }
+    }
+}
