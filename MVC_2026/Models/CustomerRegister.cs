65558c06070547da6970ee8151bf0acb02499566
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-30 17:09:36 +0530

    Add Required filed

---
+++ MVC_2026/Models/CustomerRegister.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC2026.Models
+{
+    public class CustomerRegister
+    {
+        [Required]
+        public string Custm_name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be exactly 10 digits")]
+        public string Mobile { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        public decimal? Latitude { get; set; }
+
+        public decimal? Longitude { get; set; }
+    }
+}
