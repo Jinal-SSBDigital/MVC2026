f18ad7ca0ee53b20f39aab7954b6b29022fa4cc5
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-31 17:45:46 +0530

    session add

--- MVC_2026/Program.cs
+++ MVC_2026/Program.cs
@@ -8,6 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbCon")));
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
@@ -38,11 +47,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=index}");
+    //pattern: "{controller=Account}/{action=Login}/{id?}");
+//pattern: "{controller=Museum}/{action=Location}/{id?}");
 
 app.Run();
