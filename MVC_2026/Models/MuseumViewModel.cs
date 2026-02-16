3ae6e71c813a8237687b821980a4a9e486fd5b2b
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-02-16 17:37:30 +0530

    Add Model for Museum

---
+++ MVC_2026/Models/MuseumViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVC2026.Models
+{
+    public class MuseumViewModel
+    {
+        public List<SelectListItem> States { get; set; }
+
+        public int SelectedStateId { get; set; }
+
+    }
+}
