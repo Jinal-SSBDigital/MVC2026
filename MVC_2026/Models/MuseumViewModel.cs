23af9f638ceea1b099e89b96d69bf8cd2e7d7e03
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-02-17 16:19:15 +0530

    Add cls for dropdown

--- MVC_2026/Models/MuseumViewModel.cs
+++ MVC_2026/Models/MuseumViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
 
 namespace MVC2026.Models
 {
@@ -6,7 +7,11 @@ namespace MVC2026.Models
     {
         public List<SelectListItem> States { get; set; }
 
-        public int SelectedStateId { get; set; }
+        public List<SelectListItem> MuseumCategory { get; set; }
+
+        public int? SelectedCategoryId { get; set; }
+
+        public int? SelectedStateId { get; set; }
 
     }
 }
