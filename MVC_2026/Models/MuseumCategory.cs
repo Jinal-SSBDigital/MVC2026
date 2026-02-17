f08e36d7279a007001ffa95ff0f26e8d500b8d20
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-02-17 16:19:23 +0530

    Add new cls for category

---
+++ MVC_2026/Models/MuseumCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC2026.Models
+{
+    public class MuseumCategory
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
