6bc25c883126fdc93c2127ca17c426e2a0069f40
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-02-18 15:35:21 +0530

    Add states

---
+++ MVC_2026/Models/States.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC2026.Models
+{
+    public class States
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+    }
+}
